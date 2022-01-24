eaa19dbf4a842724708a0d1b04bcf40e4156c7ff
benedettagns <benedetta.genise01@gmail.com>
2022-01-24 15:36:45 +0100

    correzione rigo nove js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ new Vue({
     mounted (){
         for(let i= 0; i < 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
-                this.emails.push(result.data.response) 
+                this.email.push(result.data.response) 
             })
         }
     },
